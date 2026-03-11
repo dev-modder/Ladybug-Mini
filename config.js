b370e9b0d7ad6c47eaaa877347f3545ac858a8f3
dev-modder <modderx7@gmail.com>
2026-03-11 15:06:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerNumber: ['91xxxxxxxxxxx','2637868310191'], // Add your number without + or spaces (e.g., 919876543210)
     ownerName: ['Ladybug Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
